47cdcfef0b5ab620bee38a271aecbfd02b25e062
Gabriel Altomani <altomanigabriel@gmail.com>
2023-08-19 16:52:25 -0300

    tela login e cadastro final sem responsividade

--- js/botao-voltar.js
+++ js/botao-voltar.js
@@ -12,5 +12,5 @@ function animatePageBack() {
     document.body.classList.add("fade-out"); // Adiciona a classe para ativar a animação
     setTimeout(() => {
         window.history.back();
-    }, 500); // Aguarda 500ms antes de voltar à página anterior
+    }, 100); // Aguarda 500ms antes de voltar à página anterior
 }
