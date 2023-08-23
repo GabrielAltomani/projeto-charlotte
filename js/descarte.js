b69ff626c1b625b445fbf33df66b017cd071e105
Gabriel Altomani <altomanigabriel@gmail.com>
2023-08-23 20:15:44 -0300

    alert home

--- js/descarte.js
+++ js/descarte.js
@@ -1,38 +1,58 @@
-var isClicked = false;
+// Variável para rastrear se o countdown está ativo ou não
+var isCountdownActive = false;
+
+// Variável para armazenar o ID do timeout
 var timeoutId;
 
+// Event listener para o botão "Realizar Descarte"
 document.getElementById("descarte").addEventListener("click", function() {
   var botao = document.getElementById("descarte");
-
-  if (isClicked) {
-    // Reverter o botão ao estado original
-    botao.innerHTML = "Realizar Descarte";
-    botao.classList.remove("clicked");
-    clearTimeout(timeoutId); // Cancelar o timeout ativo
+  
+  // Verifica se o countdown está ativo ou não
+  if (isCountdownActive) {
+    botao.innerHTML = "Realizar Descarte"; // Retorna o texto original ao botão
+    botao.classList.remove("clicked"); // Remove a classe "clicked"
+    clearTimeout(timeoutId); // Cancela o timeout ativo
   } else {
-    // Mudar o botão para o estado "Descarte Realizado!" e iniciar o countdown
-    botao.classList.add("clicked");
-    startCountdown(botao, 10000); // Iniciar o countdown
+    document.getElementById("alertOverlay").style.display = "flex"; // Mostra o overlay de alerta
   }
 
-  isClicked = !isClicked; // Alternar o estado do botão
+  // Alterna o estado do countdown
+  isCountdownActive = !isCountdownActive;
 });
 
+// Event listener para o botão "Cancelar" no alerta
+document.getElementById("cancelButton").addEventListener("click", function() {
+  document.getElementById("alertOverlay").style.display = "none"; // Esconde o overlay de alerta
+});
+
+// Event listener para o botão "Entendi" no alerta
+document.getElementById("confirmButton").addEventListener("click", function() {
+  document.getElementById("alertOverlay").style.display = "none"; // Esconde o overlay de alerta
+
+  var botao = document.getElementById("descarte");
+  botao.classList.add("clicked"); // Adiciona a classe "clicked" ao botão
+  startCountdown(botao, 10000); // Inicia o countdown com duração de 10000 milissegundos (10 segundos)
+
+  isCountdownActive = true; // Atualiza o estado do countdown
+});
+
+// Função para iniciar o countdown
 function startCountdown(button, duration) {
   var seconds = duration / 1000;
 
+  // Função para atualizar o texto do botão com a contagem regressiva
   function updateText() {
     if (seconds > 0) {
-      // Atualizar o texto do botão para mostrar a contagem regressiva
       button.innerHTML = "Tempo para descarte: " + " (" + seconds + "s)";
       seconds--;
-      timeoutId = setTimeout(updateText, 1000); // Atualizar a cada segundo
+      timeoutId = setTimeout(updateText, 1000); // Atualiza a cada segundo
     } else {
-      // Quando o countdown chega a zero, voltar ao estado normal do botão
+      // Quando o countdown chega a zero, retorna o botão ao estado original
       button.innerHTML = "Realizar Descarte";
       button.classList.remove("clicked");
     }
   }
 
-  updateText(); // Iniciar a atualização do texto
+  updateText(); // Inicia a atualização do texto
 }
